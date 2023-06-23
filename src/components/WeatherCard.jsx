149a69f78d1001b45553fe15ef3157960d64bb0a
Farhaan Malik <109364386+farhaanmalik@users.noreply.github.com>
2023-06-23 20:11:55 +0530

    Update WeatherCard.jsx

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -37,7 +37,7 @@ const WeatherCard = ({ tempInfo }) => {
                     setWeatherState("rainy");
                     break;
                 case "Thunderstorm":
-                    setWeatherState("lightning");
+                    setWeatherState("lightening");
                     break;
                 default:
                     setWeatherState("foggy")
